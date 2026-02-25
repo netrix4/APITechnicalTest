54533aff0354856bacedbb36252b950337d94912
Mario <netrix4@gmail.com>
2026-02-25 13:58:47 -0800

    Add __tablename__ property to map with table name in db

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, Numeric
+from database import Base
 
-class Product(BaseModel):
-    id: int
-    name: str
-    description: str
-    price: float
+class Product(Base):
+    __tablename__ = "Product"
 
+    id = Column(Integer, nullable=False, unique=True, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    price = Column(Numeric)
